c01f3311b44ee66b0fbf3fb0075f396179cf5d97
publicguard <dbstndh12@gmail.com>
2024-02-28 20:35:03 +0900

    프로젝트 생성

---
+++ src/test/java/com/ott/reelpick/ReelpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ott.reelpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReelpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
