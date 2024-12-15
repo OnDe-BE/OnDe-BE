419541c4255b38d4616c8cadd8f79641564e3667
whogodemo <anabel604@ewhain.net>
2024-12-15 22:25:15 +0900

    CORS 허용

--- src/main/java/com/ott/onde/config/WebMvcConfig.java
+++ src/main/java/com/ott/onde/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://localhost:3000", "https://localhost:8080","https://ottsideproject.github.io")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://localhost:3000", "https://localhost:8080","https://ondemandia.com")
                 .allowCredentials(true)
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
