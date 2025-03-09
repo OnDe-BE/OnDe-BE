70edd6fda7bafa76f57d26930434eec3b264c1a9
Dohaseok <103503302+Dohaseok@users.noreply.github.com>
2025-03-09 18:26:11 +0900

    Update User.java

--- src/main/java/com/ott/onde/user/entity/User.java
+++ src/main/java/com/ott/onde/user/entity/User.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class User implements UserDetails {
     @Id
     @Column(name = "user_code")
-    private String userId;
+    private String userCode;
     private String id;
 
     @Column(name = "password")
